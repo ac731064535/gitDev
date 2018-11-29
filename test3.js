dd5a635d98a2eaa0957b07ffdd37086b40d6d840
achen <731064535@qq.com>
2018-11-29 15:17:52 +0800

    masterxin

---
+++ test3.js
@@ -0,0 +1,20 @@
+var s = {name: 'sd', ord: 111};
+console.log(s.name)
+for (var key in s){
+	delete s.key
+	console.log(key,s[key],s.key)
+}
+console.log(s);
+
+setTimeout(function(){
+	console.log('sss')
+},2000)
+
+// dev1 开发
+setTimeout(function(){
+	console.log('dev1')
+},1000)
+function dev2(){
+	console.log('dev2')
+}
+
